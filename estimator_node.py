591f6fd2e11ccdd517bd82c2338d581e119a5130
Malcolm Benedict <mmbenedi@mtu.edu>
2026-01-29 18:00:42 -0500

    First draft of odom message publlisher

--- estimator_node.py
+++ estimator_node.py
@@ -40,9 +40,31 @@ class DeadReckoner(Node):
         odom = Odometry()
         odom.pose.pose.position.x = self.cmd_vel_x
         odom.pose.pose.position.y = self.cmd_vel_y
-        odom.pose.pose.orientation.
 
+        w,x,y,z = convertEulerToQuaternion(0,0,self.cmd_vel_t)
+        odom.pose.pose.orientation.w = w
+        odom.pose.pose.orientation.x = x
+        odom.pose.pose.orientation.y = y
+        odom.pose.pose.orientation.z = z
         
+def convertEulerToQuaternion(roll,pitch,yaw):
+    # deal with quaternions, based on code from
+    # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Euler_angles_(in_3-2-1_sequence)_to_quaternion_conversion
+    # This could be done more elegantly with a service call, but the instructions state only one python file
+    
+    cr = m.cos(roll * 0.5)
+    sr = m.sin(roll * 0.5)
+    cp = m.cos(pitch * 0.5)
+    sp = m.sin(pitch * 0.5)
+    cy = m.cos(yaw * 0.5)
+    sy = m.sin(yaw * 0.5)
+
+    w = cr * cp * cy + sr * sp * sy
+    x = sr * cp * cy - cr * sp * sy
+    y = cr * sp * cy + sr * cp * sy
+    z = cr * cp * sy - sr * sp * cy
+
+    return w,x,y,z
 
 def main(args=None):
     rclpy.init(args=args)
