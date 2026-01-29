c9cd7a4991cdb00c63412d7b7ba5c3d90208c89b
Malcolm Benedict <mmbenedi@mtu.edu>
2026-01-29 17:18:13 -0500

    First draft of dead reckoning portion

--- estimator_node.py
+++ estimator_node.py
@@ -1,9 +1,10 @@
 import rclpy
+import math as m
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 from nav_msgs.msg import Path
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import Imu
 
 class DeadReckoner(Node):
@@ -14,13 +15,35 @@ class DeadReckoner(Node):
 
         #self.dead_reckoning_odom_publisher = self.create_publisher(Odometry, '/dead_reckoning/odom', 10)
 
-        self.cmd_vel_x = 0
-        self.cmd_vel_y = 0
-        self.cmd_vel_t = 0
+        self.cmd_vel_x = 0.0
+        self.cmd_vel_y = 0.0
+        self.cmd_vel_t = 0.0
+        self.cmd_vel_v = 0.0
+        self.cmd_vel_w = 0.0
+        self.deltaT = 0.0
+        self.last_timestamp = 0.0
 
         qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
-        self.subscription = self.create_subscription(Twist,'/cmd_vel', self.cmd_vel_callback, qos_profile)
+        self.subscription = self.create_subscription(TwistStamped,'/cmd_vel', self.cmd_vel_callback, qos_profile)
+
+    def cmd_vel_callback(self,msg):
+        #calculate current state est.
+        self.deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nsec) - self.last_timestamp
+        self.cmd_vel_x = self.cmd_vel_x + (self.cmd_vel_v * m.cos(self.cmd_vel_t) * self.deltaT)
+        self.cmd_vel_y = self.cmd_vel_y + (self.cmd_vel_v * m.sin(self.cmd_vel_t) * self.deltaT)
+        self.cmd_vel_t = self.cmd_vel_t + (self.cmd_vel_w * self.deltaT)
+        #update values for next timestamp
+        self.cmd_vel_v = msg.twist.linear.x
+        self.cmd_vel_w = msg.twist.angular.z
+        self.last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nsec)
+        #publish values
+        odom = Odometry()
+        odom.pose.pose.position.x = self.cmd_vel_x
+        odom.pose.pose.position.y = self.cmd_vel_y
+        odom.pose.pose.orientation.
+
         
+
 def main(args=None):
     rclpy.init(args=args)
     estimator_node = DeadReckoner()
