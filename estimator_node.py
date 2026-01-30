5cee6acc172eca0748d7519adf4e0f4ae00d5974
Malcolm Benedict <mmbenedi@mtu.edu>
2026-01-30 12:39:29 -0500

    First draft of path publisher

--- estimator_node.py
+++ estimator_node.py
@@ -2,9 +2,8 @@ import rclpy
 import math as m
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy
-from nav_msgs.msg import Path
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import TwistStamped
+from nav_msgs.msg import Path, Odometry
+from geometry_msgs.msg import TwistStamped, Pose
 from sensor_msgs.msg import Imu
 
 class DeadReckoner(Node):
@@ -22,31 +21,46 @@ class DeadReckoner(Node):
         self.cmd_vel_w = 0.0
         self.deltaT = 0.0
         self.last_timestamp = 0.0
+        self.path_arr = []
 
         qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
         self.subscription = self.create_subscription(TwistStamped,'/cmd_vel', self.cmd_vel_callback, qos_profile)
 
     def cmd_vel_callback(self,msg):
         #calculate current state est.
-        self.deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nsec) - self.last_timestamp
+        self.deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec) - self.last_timestamp
         self.cmd_vel_x = self.cmd_vel_x + (self.cmd_vel_v * m.cos(self.cmd_vel_t) * self.deltaT)
         self.cmd_vel_y = self.cmd_vel_y + (self.cmd_vel_v * m.sin(self.cmd_vel_t) * self.deltaT)
         self.cmd_vel_t = self.cmd_vel_t + (self.cmd_vel_w * self.deltaT)
         #update values for next timestamp
         self.cmd_vel_v = msg.twist.linear.x
         self.cmd_vel_w = msg.twist.angular.z
-        self.last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nsec)
-        #publish values
-        odom = Odometry()
-        odom.pose.pose.position.x = self.cmd_vel_x
-        odom.pose.pose.position.y = self.cmd_vel_y
+        self.last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec)
+        
+        #generate pose message
+        current_pose = Pose()
+
+        current_pose.position.x = self.cmd_vel_x
+        current_pose.position.y = self.cmd_vel_y
 
         w,x,y,z = convertEulerToQuaternion(0,0,self.cmd_vel_t)
-        odom.pose.pose.orientation.w = w
-        odom.pose.pose.orientation.x = x
-        odom.pose.pose.orientation.y = y
-        odom.pose.pose.orientation.z = z
-        
+        current_pose.orientation.w = w
+        current_pose.orientation.x = x
+        current_pose.orientation.y = y
+        current_pose.orientation.z = z
+
+        #publish odom message
+        odom = Odometry()
+        odom.pose.pose = current_pose
+
+        self.dead_reckoning_odom_publisher.publish(odom)
+
+        #publish path values
+        path = Path()
+        self.path_arr.append(current_pose)
+        path.poses = self.path_arr
+        self.dead_reckoning_path_publisher.publish(path)
+
 def convertEulerToQuaternion(roll,pitch,yaw):
     # deal with quaternions, based on code from
     # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Euler_angles_(in_3-2-1_sequence)_to_quaternion_conversion
