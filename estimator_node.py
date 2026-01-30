39ca1e76aa132968db09dd8cca3d76434e29dc22
Malcolm Benedict <mmbenedi@mtu.edu>
2026-01-30 14:13:23 -0500

    First draft of IMU code

--- estimator_node.py
+++ estimator_node.py
@@ -24,22 +24,36 @@ class DeadReckoner(Node):
         #define publishers
         self.dead_reckoning_path_publisher = self.create_publisher(Path, '/dead_reckoning/path', 10)
         self.dead_reckoning_odom_publisher = self.create_publisher(Odometry, '/dead_reckoning/odom', 10)
+        self.imu_odom_publisher = self.create_publisher(Odometry, '/imu_integration/odom', 10)
+        self.imu_path_publisher = self.create_publisher(Path, '/imu_integration/path', 10)
 
-        #self.dead_reckoning_odom_publisher = self.create_publisher(Odometry, '/dead_reckoning/odom', 10)
-
-        #initialize states to zero
+        #initialize cmd_vel_callback states to zero
         self.cmd_vel_x = 0.0
         self.cmd_vel_y = 0.0
         self.cmd_vel_t = 0.0
         self.cmd_vel_v = 0.0
         self.cmd_vel_w = 0.0
-        self.deltaT = 0.0
-        self.last_timestamp = 0.0
-        self.dead_reckoning_path_arr = []
+        self.cmd_vel_deltaT = 0.0
+        self.cmd_vel_last_timestamp = 0.0
+        self.cmd_vel_path_arr = []
+
+        #initialize imu_callback states to zero
+        self.imu_x = 0.0
+        self.imu_y = 0.0
+        self.imu_t = 0.0
+        self.imu_vx = 0.0
+        self.imu_vy = 0.0
+        self.imu_w = 0.0
+        self.imu_ax = 0.0
+        self.imu_ay = 0.0
+        self.imu_deltaT = 0.0
+        self.imu_last_timestamp = 0.0
+        self.imu_path_arr = []
 
         #define subscribers
         qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
-        self.subscription = self.create_subscription(TwistStamped,'/cmd_vel', self.cmd_vel_callback, qos_profile)
+        self.cmd_vel_subscription = self.create_subscription(TwistStamped,'/cmd_vel', self.cmd_vel_callback, qos_profile)
+        self.imu_subscription = self.create_subscription(Imu,'/imu', self.imu_callback, qos_profile)
 
     def cmd_vel_callback(self,msg):
         """
@@ -50,15 +64,15 @@ class DeadReckoner(Node):
         :param msg: the incoming TwistStamped /cmd_vel message.
         """
         #calculate current state est.
-        self.deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec) - self.last_timestamp
-        self.cmd_vel_x = self.cmd_vel_x + (self.cmd_vel_v * m.cos(self.cmd_vel_t) * self.deltaT)
-        self.cmd_vel_y = self.cmd_vel_y + (self.cmd_vel_v * m.sin(self.cmd_vel_t) * self.deltaT)
-        self.cmd_vel_t = self.cmd_vel_t + (self.cmd_vel_w * self.deltaT)
+        self.cmd_vel_deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec) - self.cmd_vel_last_timestamp
+        self.cmd_vel_x = self.cmd_vel_x + (self.cmd_vel_v * m.cos(self.cmd_vel_t) * self.cmd_vel_deltaT)
+        self.cmd_vel_y = self.cmd_vel_y + (self.cmd_vel_v * m.sin(self.cmd_vel_t) * self.cmd_vel_deltaT)
+        self.cmd_vel_t = self.cmd_vel_t + (self.cmd_vel_w * self.cmd_vel_deltaT)
 
         #update values for next timestamp
         self.cmd_vel_v = msg.twist.linear.x
         self.cmd_vel_w = msg.twist.angular.z
-        self.last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec)
+        self.cmd_vel_last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec)
         
         #generate pose message
         current_pose = PoseStamped()
@@ -85,17 +99,59 @@ class DeadReckoner(Node):
         path = Path()
         path.header.stamp = Clock().now().to_msg()
         path.header.frame_id = 'odom'
-        self.dead_reckoning_path_arr.append(current_pose)
-        path.poses = self.dead_reckoning_path_arr
+        self.cmd_vel_path_arr.append(current_pose)
+        path.poses = self.cmd_vel_path_arr
         self.dead_reckoning_path_publisher.publish(path)
 
+    def imu_callback(self,msg):
+        self.imu_deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec) - self.imu_last_timestamp
+        self.imu_x = self.imu_x + (self.imu_vx * self.imu_deltaT)
+        self.imu_y = self.imu_y + (self.imu_vy * self.imu_deltaT)
+        self.imu_vx = self.imu_vx + (self.imu_ax * self.imu_deltaT)
+        self.imu_vy = self.imu_vy + (self.imu_ay * self.imu_deltaT)
+        self.imu_t = self.imu_t + (self.imu_w * self.imu_deltaT)
+
+        #update values for next timestamp
+        self.imu_ax = msg.linear_acceleration.x
+        self.imu_ay = msg.linear_acceleration.y
+        self.imu_w = msg.angular_velocity.z
+        self.cmd_vel_last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec)
+
+        #generate pose message
+        current_pose = PoseStamped()
+        current_pose.header.stamp = Clock().now().to_msg()
+        current_pose.header.frame_id = 'odom'
+        current_pose.pose.position.x = self.imu_x
+        current_pose.pose.position.y = self.imu_y
+
+        #get orientation quaternion
+        w,x,y,z = convertEulerToQuaternion(0,0,self.imu_t)
+        current_pose.pose.orientation.w = w
+        current_pose.pose.orientation.x = x
+        current_pose.pose.orientation.y = y
+        current_pose.pose.orientation.z = z
+
+        #publish odom message
+        odom = Odometry()
+        odom.header.stamp = Clock().now().to_msg()
+        odom.header.frame_id = 'odom'
+        odom.pose.pose = current_pose.pose
+        self.imu_odom_publisher.publish(odom)
+
+        #publish path values
+        path = Path()
+        path.header.stamp = Clock().now().to_msg()
+        path.header.frame_id = 'odom'
+        self.imu_path_arr.append(current_pose)
+        path.poses = self.imu_path_arr
+        self.imu_path_publisher.publish(path)
 
 def convertEulerToQuaternion(roll,pitch,yaw)-> tuple[float,float,float,float]:
     """
     Helper function to convert Euler angles to a Quaternion
     based on code from
     https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles#Euler_angles_(in_3-2-1_sequence)_to_quaternion_conversion
-    This could be done more elegantly with a service call, but the instructions state only one python file
+    This could probably be done more elegantly with a service call, but the instructions state only one python file
 
     :param roll: Rotation about the X axis
     :param pitch: Rotation about the Y axis
