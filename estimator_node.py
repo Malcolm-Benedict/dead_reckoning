a73dd3d4fa614b6d5ffb1c7fbb6784aa99450a31
Malcolm Benedict <mmbenedi@mtu.edu>
2026-02-01 15:04:09 -0500

    Saving work

--- estimator_node.py
+++ estimator_node.py
@@ -52,6 +52,10 @@ class DeadReckoner(Node):
         self.imu_x = 0.0
         self.imu_y = 0.0
         self.imu_path_arr = []
+        self.First = True
+        self.imu_offset_ax = 0.0
+        self.imu_offset_ay = 0.0
+        self.imu_offset_w = 0.0
 
         #define subscribers
         subscription_qos_profile = QoSProfile(history=HistoryPolicy.KEEP_ALL, reliability=ReliabilityPolicy.BEST_EFFORT)
@@ -107,19 +111,27 @@ class DeadReckoner(Node):
         self.dead_reckoning_path_publisher.publish(path)
 
     def imu_callback(self,msg):
+        if(self.First):
+            self.imu_offset_ax = msg.linear_acceleration.x
+            self.imu_offset_ay = msg.linear_acceleration.y
+            self.First = False
+
         self.imu_deltaT = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec) - self.imu_last_timestamp
         self.imu_ax = (self.imu_acc_x * m.cos(self.imu_t)) - (self.imu_acc_y * m.sin(self.imu_t))
         self.imu_ay = (self.imu_acc_x * m.sin(self.imu_t)) + (self.imu_acc_y * m.cos(self.imu_t))
         self.imu_x = self.imu_x + (self.imu_vx * self.imu_deltaT)
         self.imu_y = self.imu_y + (self.imu_vy * self.imu_deltaT)
         self.imu_t = self.imu_t + (self.imu_w * self.imu_deltaT)
+        print(self.imu_t)
+        #self.imu_t = self.imu_t % (2*m.pi)
         self.imu_vx = self.imu_vx + (self.imu_ax * self.imu_deltaT)
         self.imu_vy = self.imu_vy + (self.imu_ay * self.imu_deltaT)
         
         #update for next time frame
-        self.imu_acc_x = msg.linear_acceleration.x
-        self.imu_acc_y = msg.linear_acceleration.y
-        self.imu_w = msg.angular_velocity.z
+        self.imu_acc_x = msg.linear_acceleration.x - self.imu_offset_ax 
+        self.imu_acc_y = msg.linear_acceleration.y - self.imu_offset_ay
+        self.imu_w = msg.angular_velocity.z 
+        #print(self.imu_w)
         self.imu_last_timestamp = msg.header.stamp.sec + (0.000000001 * msg.header.stamp.nanosec)
 
         #generate pose message
